511f0895078ebd648777f53c536fcfbb2571b7b4
Nikita Sharma <51536421+niksisharma@users.noreply.github.com>
2022-05-21 16:13:22 +0530

    Create Brute Force.py

---
+++ Algorithms/02 Search/01 Ice Cream Parlor/Brute Force.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def icecreamParlor(m, arr):
+    for i in range(len(arr)):
+        for j in range(i+1,len(arr)):
+            if arr[i] + arr[j] == m:
+                return [i+1,j+1]
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        m = int(input().strip())
+
+        n = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = icecreamParlor(m, arr)
+
+        fptr.write(' '.join(map(str, result)))
+        fptr.write('\n')
+
+    fptr.close()
