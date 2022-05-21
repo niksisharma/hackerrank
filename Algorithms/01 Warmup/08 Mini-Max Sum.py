735d482398793490d7d6fdf50278845dee502139
Nikita Sharma <51536421+niksisharma@users.noreply.github.com>
2022-05-21 12:03:15 +0530

    Create 08 Mini-Max Sum.py

---
+++ Algorithms/01 Warmup/08 Mini-Max Sum.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def miniMaxSum(arr):
+    minsum = sum(arr) - max(arr)
+    maxsum = sum(arr) - min(arr)
+    print(minsum,maxsum)
+
+if __name__ == '__main__':
+
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
